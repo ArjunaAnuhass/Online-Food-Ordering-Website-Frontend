2c15ced7eedfaf9062bb4cc769e0e78dc11ae8b9
Arjuna Anuhass <anuhass2017@gmail.com>
2024-06-05 10:20:18 +0530

    Favourite Profile completed

--- src/components/Profile/Favorites.jsx
+++ src/components/Profile/Favorites.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import RestaurantCard from '../Restaurant/RestaurantCart'
 
 export const Favorites = () => {
   return (
-    <div>Favorites</div>
+    <div>
+      <h1 className='py-5 text-xl font-semibold text-center'>My Favorites</h1>
+
+      <div className='flex flex-wrap gap-5 justify-center'>
+        {[1,1,1].map((item)=><RestaurantCard/>)}
+      </div>
+    </div>
   )
 }
